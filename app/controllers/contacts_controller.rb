e6c4f8168f108db88e272c0ab320a365adc98158
Sam Gillette <sam@samgillette.com>
2017-07-04 17:32:00 +0000

    Added send email function in contacts controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,6 +6,10 @@ class ContactsController < ApplicationController
     def create
        @contact = Contact.new(contact_params)
        if @contact.save
+           name = params[:contact][:name]
+           email = params[:contact][:email]
+           message = params[:contact][:message]
+           ContactMailer.contact_email(name, email, message).deliver
            flash[:success] = "Message sent."
            redirect_to new_contact_path
        else
