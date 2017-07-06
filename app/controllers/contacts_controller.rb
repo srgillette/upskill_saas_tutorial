a572223a7a1fd85efe59de50bbc45ecef2c25938
Sam Gillette <sam@samgillette.com>
2017-07-06 04:46:46 +0000

    Added mailer settings in environment file

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -20,6 +20,6 @@ class ContactsController < ApplicationController
     
     private
         def contact_params
-           params.require(:contact).permit(:name, :email, :message) 
+           params.require(:contact).permit(:name, :email, :message)
         end
 end
